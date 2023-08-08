2473b9c469965af87934b8e6fa3cb8239ebe47ea
GU Jun <guj2023@outlook.jp>
2023-08-08 08:25:52 +0800

    communication function basicly completed

--- src/uart_converter/uart_converter/uart_converter_node.py
+++ src/uart_converter/uart_converter/uart_converter_node.py
@@ -8,7 +8,7 @@ from omniwheg_interfaces.msg import IMU, Wheel
 
 
 
-serial_port = '/dev/ttyUSB1'
+serial_port = '/dev/ttyUSB0'
 baud_rate = 9600
 robot_data = '000'
 
@@ -17,55 +17,73 @@ class UartConverter(Node):
         super().__init__('robot_status_publisher')
         self.imuPublisher = self.create_publisher(IMU, 'pose', 10)
         self.wheelStatusPublisher = self.create_publisher(Wheel, 'wheelStatus', 10)
-        timer_period = 0.5
+        timer_period = 0.1
         self.timer = self.create_timer(timer_period, self.timer_callback)
         # Establish a connection to the serial port
         self.ser = serial.Serial(serial_port, baud_rate, timeout=1)
+
+    def verifyCheckLength(self, data):
+        if data:
+            lengthInfo = data.split(" ")[-1]
+            if lengthInfo:
+                if len(lengthInfo) >= 1 and lengthInfo.isdigit():
+                    if len(data[:-len(lengthInfo)].strip()) == int(lengthInfo):
+                        return True
+                    else:
+                        return False
+                else:
+                    return False
+            else:   
+                return False
+        else:   
+            return False
+
+
     def imuInfoSeprate(self, raw_data):
         imuMsg = IMU()
-        temArray = raw_data.split("\t")
-        for item in temArray:
-            if item[0] == 'x':
-                imuMsg.angle_x = float(item[2:].strip())
-            elif item[0] == 'y':
-                imuMsg.angle_y = float(item[2:].strip())
-            elif item[0] == 'z':
-                imuMsg.angle_z = float(item[2:].strip())
-            else:
-                self.get_logger().info("imu message error")
+        temArray = raw_data.split(" ")
+        try:
+            imuMsg.angle_x = float(temArray[1])
+            imuMsg.angle_y = float(temArray[2])
+            imuMsg.angle_z = float(temArray[3])
+        except:
+            self.get_logger().info("imu message error")
+            return None
         return imuMsg
 
     def wheelInfoSeprate(self, raw_data):
         wheelMsg = Wheel()
-        wheelMsg.circle = 0
-        temArray = raw_data.split("\t")
-        for item in temArray:
-            if item[0] == 'i':
-                wheelMsg.id = int(item[2:].strip())
-            elif item[0] == 's':
-                wheelMsg.speed = float(item[2:].strip())
-            elif item[0:3] == 'PWM':
-                wheelMsg.pwm = float(item[4:].strip())
-            elif item[0:1] == 'c:':
-                wheelMsg.current = float(item[2:].strip())
-            elif item[0] == 'p':
-                wheelMsg.position = float(item[2:].strip())
-            elif item[0:1] == 'ci':
-                wheelMsg.circle = int(item[3:].strip())
-            else:
-                self.get_logger().info("wheel status message error")
+        temArray = raw_data.split(" ")
+        try:
+            wheelMsg.id = int(temArray[1])
+            wheelMsg.speed = float(temArray[2])
+            wheelMsg.pwm = float(temArray[3])
+            wheelMsg.current = float(temArray[4])
+            wheelMsg.position = float(temArray[5])
+            wheelMsg.circle = int(temArray[6])
+        except:
+            self.get_logger().info("wheel message error")
+            return None
         return wheelMsg
 
 
     def timer_callback(self):
         # Read data from the serial port
-        robot_data = self.ser.readline().decode('utf-8').strip()
+        try:
+            robot_data = self.ser.readline().decode('utf-8').strip()
+        except:
+            self.get_logger().info("uart read error")
+            return None
         if robot_data:
             self.get_logger().info('raw message: "%s" ' % robot_data)
-            if robot_data[0] == 'x':
+            if self.verifyCheckLength(robot_data):
+                robot_data = robot_data[:-len(robot_data.split(" ")[-1])]
+            else:
+                self.get_logger().info("message length error")
+            if robot_data[0] == 'i':
                 imuMsg = self.imuInfoSeprate(robot_data)
                 self.imuPublisher.publish(imuMsg)
-            elif robot_data[0] == 'i':
+            elif robot_data[0] == 'w':
                 wheelMsg = self.wheelInfoSeprate(robot_data)
                 self.wheelStatusPublisher.publish(wheelMsg)
             else:
